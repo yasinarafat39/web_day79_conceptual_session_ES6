4ed9a512e50fbdcf2e8a139b6bcf79605f095313
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-29 23:21:47 +0600

    logical operator

---
+++ logical.js
@@ -0,0 +1,20 @@
+// logical operator (and or) && || 
+
+const money = false;
+
+// and && 
+// left && right
+/* if left side is true than work right side otherwise not */
+
+console.log(money && "Bus e jabo");
+
+// or ||
+// left || right
+/* if left side not work than right side work otherwise not */
+const bus = "Bus e jabo";
+const ricksha = "Rickshai jabo";
+
+console.log(money === true? bus : ricksha);
+
+const result = money === true? "Rikshai jabo" : "Hete Hete jabo";
+console.log(result);
