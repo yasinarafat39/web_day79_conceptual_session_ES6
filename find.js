10c338183e377927590220e6d28e71ca1bd6de6d
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-30 11:40:30 +0600

    find

---
+++ find.js
@@ -0,0 +1,19 @@
+const oddNumbers = [1, 3, 5, 10, 7, 9, 8];
+
+// let find = null;
+
+// for(let item of oddNumbers){
+//     if(item > 5){
+//         find = item;
+//         break;
+//     }
+// }
+
+// console.log(find);
+
+
+// find()
+/* find() method return only one element that match as a first */
+/* find() can take multiple conditions */
+const result = oddNumbers.find((item) => item > 5 && item < 10 && item % 2 === 0);
+console.log(result);
